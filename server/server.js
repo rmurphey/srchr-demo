96f5a4251b0b11bcb7a47c4fcfe3d526dcee4a85
Rebecca Murphey <rmurphey@gmail.com>
2012-04-06 15:46:25 -0400

    move server code out of bin/server

---
+++ server/server.js
@@ -0,0 +1,67 @@
+var express   = require('express'),
+    fs        = require('fs'),
+    search    = require('server/search.js'),
+    favs      = require('server/favorites.js'),
+    site      = express.createServer(),
+    staticDir = express['static'],
+    _         = require('underscore');
+
+module.exports = function(opts) {
+  opts = _.extend({
+    port : 4444
+  }, opts || {});
+
+  site.configure(function() {
+    [ 'app', 'lib', 'assets' ].forEach(function(dir) {
+      site.use('/' + dir, staticDir('./' + dir));
+    });
+    site.use(express.bodyParser());
+  });
+
+  site.get("/", function(req, res) {
+    fs.createReadStream('./index.html').pipe(res);
+  });
+
+  site.get("/search/:term", function(req, res) {
+    var term = req.params.term;
+
+    search(req.params.term).then(
+      function(data) {
+        res.end(JSON.stringify(data));
+      },
+      function(statusCode) {
+        throw new Error();
+      }
+    );
+  });
+
+  site.post("/favorites", function(req, res) {
+    var fav = JSON.parse(req.body.favorite);
+    var id = favs.add(fav);
+    res.end(JSON.stringify({ id : id }));
+  });
+
+  site.put("/favorites/:id", function(req, res) {
+    var fav = JSON.parse(req.body.favorite);
+    var id = favs.update(req.params.id, fav);
+    res.end(JSON.stringify({ id : id }));
+  });
+
+  site.delete("/favorites/:id", function(req, res) {
+    favs.remove(req.params.id);
+    res.end(JSON.stringify({ success : true }));
+  });
+
+  site.get("/favorites", function(req, res) {
+    res.end(JSON.stringify(favs.get()));
+  });
+
+  site.get("/favorites/:id", function(req, res) {
+    var fav = favs.get(req.params.id);
+    res.end(JSON.stringify(fav));
+  });
+
+  // Actually listen
+  site.listen(opts.port);
+  console.log("Serving at http://localhost:" + opts.port);
+};
