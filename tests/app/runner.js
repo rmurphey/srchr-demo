6db810914d3f3ebed0dd87de7ff322bb9bd919cd
Rebecca Murphey <rmurphey@gmail.com>
2012-04-09 13:13:00 -0400

    allow running one suite at a time

--- tests/app/runner.js
+++ tests/app/runner.js
@@ -1,8 +1,17 @@
-require([
-  'tests/app/components/base.js',
-  'tests/app/components/searchForm',
-  'tests/app/components/results',
-  'tests/app/components/recentSearches'
-], function() {
+var tests;
+
+if (window.location.hash) {
+  tests = location.hash.replace('#', '').split(',');
+  console.log(tests);
+} else {
+  tests = [
+    'tests/app/components/base.js',
+    'tests/app/components/searchForm',
+    'tests/app/components/results',
+    'tests/app/components/recentSearches'
+  ]
+}
+
+require(tests, function() {
   mocha.run();
 });
