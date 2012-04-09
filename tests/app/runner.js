ed9139e7de31ada7e84567c35afef5c4d582894c
Rebecca Murphey <rmurphey@gmail.com>
2012-04-09 13:22:39 -0400

    allow navigation to individual tests

--- tests/app/runner.js
+++ tests/app/runner.js
@@ -2,14 +2,25 @@ var tests;
 
 if (window.location.hash) {
   tests = location.hash.replace('#', '').split(',');
-  console.log(tests);
 } else {
   tests = [
-    'tests/app/components/base.js',
+    'tests/app/components/base',
     'tests/app/components/searchForm',
     'tests/app/components/results',
     'tests/app/components/recentSearches'
   ]
+}
+
+if (Array.prototype.forEach) {
+  var $filter = $('<div id="filter-tests"></div>').insertBefore('#mocha');
+
+  tests.forEach(function(t) {
+    $filter.append('<li><a href="#' + t + '">' + t + '</a></li>');
+  });
+
+  $filter.find('a').click(function() {
+    window.location.reload();
+  });
 }
 
 require(tests, function() {
