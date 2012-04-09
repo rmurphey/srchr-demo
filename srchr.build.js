3bab484db248e1ee8282c66f29c3131bd0d9083e
Rebecca Murphey <rmurphey@gmail.com>
2012-04-09 10:31:42 -0400

    build profile

---
+++ srchr.build.js
@@ -0,0 +1,31 @@
+({
+  baseUrl : 'app',
+  paths : {
+    lib : '../lib',
+    plugins : '../lib/plugins',
+    app : '.',
+
+    jquery : '../lib/jquery',
+    underscore : '../lib/underscore',
+    backbone : '../lib/backbone',
+
+    use : '../lib/plugins/use',
+    text : '../lib/plugins/text'
+  },
+
+  use : {
+    underscore : {
+      attach : '_'
+    },
+    backbone : {
+      deps : [ 'use!underscore', 'jquery' ],
+      attach : [ 'Backbone' ]
+    }
+  },
+
+  modules : [
+    {
+      name : 'app/main'
+    }
+  ]
+})
