42869bc0867f06a081d82c80694b7b178f3c4098
Rebecca Murphey <rmurphey@gmail.com>
2012-04-24 22:41:05 +0200

    remove unused var

--- app/collections/searchData.js
+++ app/collections/searchData.js
@@ -4,7 +4,6 @@ define([
   var SearchData = B.Collection.extend({
     fetch : function(opts) {
       var fetch = _.bind(B.Collection.prototype.fetch, this),
-          oldTerm = opts && opts.data && opts.data.term,
           dfd = $.Deferred();
 
       this.trigger('fetching');
