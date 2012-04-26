5dab5a0bccbad4d1168d8fa9cc833f75ff1d9acc
Rebecca Murphey <rmurphey@gmail.com>
2012-04-26 10:31:51 +0200

    commit config file

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ module.exports = {
    * In order for this app to fetch data from Flickr,
    * you will need a Flickr API key. You can obtain one here: http://www.flickr.com/services/apps/create/apply/
    */
-  flickr : 'your_api_key_here'
+  "flickr" : "14a40b6ca1482039d4692b5e05045af1"
 };
